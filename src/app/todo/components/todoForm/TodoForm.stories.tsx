bac781a2c861bbb29fddb62bcd921418b2308cc9
LimChaeJune <30399915+LimChaeJune@users.noreply.github.com>
2025-06-25 17:13:11 +0900

    chore: :truck: convention을 위한 폴더명 firstUpper 폴더로 변경

--- src/app/todo/components/todoForm/TodoForm.stories.tsx
+++ src/app/todo/components/todoForm/TodoForm.stories.tsx
@@ -1,5 +1,3 @@
-// src/app/todo/components/todoForm/todoForm.stories.tsx
-import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { userEvent, within } from '@storybook/test'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
